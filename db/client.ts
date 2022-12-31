a657df3acbb94e4906bbf7baecdf3ed17f98354e
Khoi T. Nguyen <thorwaitson@gmail.com>
2022-12-31 17:22:05 -0600

    update elastic db client

--- db/client.ts
+++ db/client.ts
@@ -1,14 +1,13 @@
 import { Client } from "@elastic/elasticsearch";
 
 const elasticClient = new Client({
-  //   cloud: {
-  //     id: process.env.ELASTIC_CLOUD_ID,
-  //   },
-  //   auth: {
-  //     username: process.env.ELASTIC_USERNAME as string,
-  //     password: process.env.ELASTIC_PASSWORD as string,
-  //   },
-  node: process.env.ELASTIC_NODE,
+  cloud: {
+    id: process.env.ELASTIC_CLOUD_ID as string,
+  },
+  auth: {
+    username: process.env.ELASTIC_USERNAME as string,
+    password: process.env.ELASTIC_PASSWORD as string,
+  },
 });
 
 export default elasticClient;
